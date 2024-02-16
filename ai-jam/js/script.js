faa8480fe1a9fe4d18c85fabb09015bb728bcbd1
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2024-02-16 13:36:13 -0500

    added a loading state manager and added canvas size globals

--- ai-jam/js/script.js
+++ ai-jam/js/script.js
@@ -7,8 +7,12 @@
 
 "use strict";
 
+const canvasWidth = 1280;
+const canvasHeight = 854;
+
+let state = "loading";
 let facePredictions = [];
-let video1;
+let video;
 let facemesh;
 let faceShift = {
     x:0,
@@ -27,12 +31,14 @@ function preload() {
  * Setup before draw
 */
 function setup() {
-    createCanvas(windowWidth,windowHeight);
+    createCanvas(canvasWidth,canvasHeight);
 
-    video1 = createCapture(VIDEO);
-    video1.hide();
+    video = createCapture(VIDEO);
+    video.hide();
 
-    facemesh = ml5.facemesh(video1, {}, function() { console.log("face model loaded!"); });
+    facemesh = ml5.facemesh(video, {}, function() { 
+        state = "running"; 
+    });
     facemesh.on(`face`, function(results) {
         facePredictions = results;
     });
@@ -43,6 +49,21 @@ function setup() {
  * Draw to canvas every frame
 */
 function draw() {
+    // Run the appropriate running state
+    switch(state){
+        case "running":
+            running();
+            break;
+        case "loading":
+            loading();
+            break;
+        default:
+            console.log("ERROR: state variable not set correctly.");
+            break;
+    }
+}
+
+function running(){
     background(0);
 
     if(facePredictions.length > 0){
@@ -63,21 +84,22 @@ function draw() {
         };
         // Calibrate the center point to the center of the canvas
         if(faceShift.x == 0 || faceShift.y == 0){
-            faceShift.x = windowWidth/2 - faceCenter.x;
-            faceShift.y = windowHeight/2 - faceCenter.y;
+            faceShift.x = canvasWidth/2 - faceCenter.x;
+            faceShift.y = canvasHeight/2 - faceCenter.y;
         }
         faceCenter.x += faceShift.x;
         faceCenter.y += faceShift.y;
 
         // Create a rectangle for the backdrop
-        let rectW = windowWidth/3 + facePredictions[0].mesh[0][2] * 5;
-        let rectH = windowHeight/3 + facePredictions[0].mesh[0][2] * 5;
+        let rectW = canvasWidth/3 + facePredictions[0].mesh[0][2] * 5;
+        let rectH = canvasHeight/3 + facePredictions[0].mesh[0][2] * 5;
         let faceRect = {
             x:faceCenter.x - rectW/2,
             y:faceCenter.y - rectH/2,
             w:rectW,
             h:rectH,
         }
+
         // Draw the shapes
         push()
         // Draw the backdrop rectangle
@@ -86,22 +108,35 @@ function draw() {
         rect(faceRect.x,faceRect.y,faceRect.w,faceRect.h);
         // Draw the walls and ceiling
         fill(100);
-        quad(0,0, windowWidth,0, faceRect.x+faceRect.w,faceRect.y, faceRect.x,faceRect.y);//Ceiling
+        quad(0,0, canvasWidth,0, faceRect.x+faceRect.w,faceRect.y, faceRect.x,faceRect.y);//Ceiling
         fill(200);
-        quad(0,0, faceRect.x,faceRect.y, faceRect.x,faceRect.y+faceRect.h, 0,windowHeight);//Left wall
-        quad(windowWidth,0, windowWidth,windowHeight, faceRect.x+faceRect.w,faceRect.y+faceRect.h, faceRect.x+faceRect.w,faceRect.y);//Right wall
+        quad(0,0, faceRect.x,faceRect.y, faceRect.x,faceRect.y+faceRect.h, 0,canvasHeight);//Left wall
+        quad(canvasWidth,0, canvasWidth,canvasHeight, faceRect.x+faceRect.w,faceRect.y+faceRect.h, faceRect.x+faceRect.w,faceRect.y);//Right wall
         fill(50);
-        quad(0,windowHeight, faceRect.x,faceRect.y+faceRect.h, faceRect.x+faceRect.w,faceRect.y+faceRect.h, windowWidth,windowHeight);//Floor
+        quad(0,canvasHeight, faceRect.x,faceRect.y+faceRect.h, faceRect.x+faceRect.w,faceRect.y+faceRect.h, canvasWidth,canvasHeight);//Floor
 
         // Draw perspective lines to the backdrop from the corners
         line(0,0,faceRect.x,faceRect.y);
-        line(windowWidth,0,faceRect.x+faceRect.w,faceRect.y);
-        line(0,windowHeight,faceRect.x,faceRect.y+faceRect.h);
-        line(windowWidth,windowHeight,faceRect.x+faceRect.w,faceRect.y+faceRect.h);
+        line(canvasWidth,0,faceRect.x+faceRect.w,faceRect.y);
+        line(0,canvasHeight,faceRect.x,faceRect.y+faceRect.h);
+        line(canvasWidth,canvasHeight,faceRect.x+faceRect.w,faceRect.y+faceRect.h);
         // Draw the center point
         fill(0,150,0);
         ellipse(faceCenter.x,faceCenter.y,5);
+
+        // ROWS
+        fill(0,150,0);
+        rect(200,canvasHeight-100,400,50);
         pop();
     }
-    
+}
+
+function loading(){
+    background(0);
+    // Draw the loading text
+    push();
+    fill(255);
+    textSize(50);
+    text("loading...",canvasWidth/2 - 100,canvasHeight/2);
+    pop();
 }
